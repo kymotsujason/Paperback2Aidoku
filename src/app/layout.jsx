504dd23be4bbffc5537c981af609e320496e82f9
Jason Yue <kymotsujason@gmail.com>
2024-09-15 11:59:00 -0700

    Add Vercel insights

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,6 @@
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -26,7 +28,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				{children}
+				<Analytics />
+				<SpeedInsights />
+			</body>
 		</html>
 	);
 }
